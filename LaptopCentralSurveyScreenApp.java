28bd98a4a55cec5e8e312ee99661d355b766b424
SciTechBoi⚡️💻🧑‍🔬 <bwang@markhamfirebirds.ca>
2024-12-16 21:34:57 -0500

    Update LaptopCentralSurveyScreenApp.java

--- LaptopCentralSurveyScreenApp.java
+++ LaptopCentralSurveyScreenApp.java
@@ -3,7 +3,7 @@ public class LaptopCentralSurveyScreenApp{
 	
 	public static void main(String[]args) {
 		
-		new LaptopCentralSurveyScreen(); //Oh shoot we need mr. fernandes's help again the setbounds method aint putting the labels where i want them to be yet again
+		new LaptopCentralSurveyScreen(); 
 	}
 	
 }
